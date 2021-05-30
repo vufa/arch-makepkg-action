41a37f59968d836fea61080ec6810137af557630
Codist <countstarlight@gmail.com>
2021-05-30 18:41:00 +0800

    install packages with yay
    Signed-off-by: Codist <countstarlight@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,11 +7,9 @@ fi
 sudo pacman -Syu --noconfirm
 #namcap PKGBUILD
 
-if [ ${#INPUT_PACKAGES[@]} -gt 0 ]; then
-    for pkg in "${INPUT_PACKAGES[@]}"; do
-      yay -S "$pkg" --noconfirm --needed --useask --gpgflags "--keyserver hkp://pool.sks-keyservers.net" || exit $?
-    done
-fi
+for pkg in $INPUT_PACKAGES; do
+    yay -S "$pkg" --noconfirm --needed --useask --gpgflags "--keyserver hkp://pool.sks-keyservers.net" || exit $?
+done
 
 #sudo makepkg -fC --syncdeps --noconfirm
 makepkg
