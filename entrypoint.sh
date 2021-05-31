289cfa2a021bab8d6a4de833ed563459a81aa6c3
Codist <countstarlight@gmail.com>
2021-05-31 11:06:28 +0800

    fixed eval scripts
    Signed-off-by: Codist <countstarlight@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ for pkg in $INPUT_PACKAGES; do
     yay -S "$pkg" --noconfirm --needed --useask --gpgflags "--keyserver hkp://pool.sks-keyservers.net" || exit $?
 done
 
-eval $INPUT_SCRIPT || exit $?
+eval $INPUT_SCRIPTS || exit $?
