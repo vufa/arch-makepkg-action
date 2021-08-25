1e947a10af92351c81f9414853efae81bc895f1a
Codist <countstarlight@gmail.com>
2021-08-25 19:15:31 +0800

    try to fix add custom repositories
    Signed-off-by: Codist <countstarlight@gmail.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ fi
 # add custom repositories to pacman.conf
 for r in $INPUT_REPOS; do
     IFS=" " read -r -a splitarr <<< "${r//=/ }"
-    sudo echo -e "\n[${splitarr[0]}]" >> /etc/pacman.conf
-    sudo echo "Server = ${splitarr[1]}" >> /etc/pacman.conf
+    echo -e "\n[${splitarr[0]}]" | sudo tee -a /etc/pacman.conf
+    echo "Server = ${splitarr[1]}" | sudo tee -a  /etc/pacman.conf
 done
 
 # update repos
